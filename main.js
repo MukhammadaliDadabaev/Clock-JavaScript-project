ff19318a16b7fbf9bbe9c9a9b7bdf66a44d4d222
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2022-04-24 19:55:45 +0500

    Clock project-example

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function showTime() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  if (hours == 0) {
+    hours;
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  const time = hours + ":" + minutes + ":" + seconds;
+
+  // document.getElementById("ClockDisplay").innerText = time;
+  document.getElementById("ClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+showTime();
